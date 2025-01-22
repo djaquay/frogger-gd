3c7ca99fe92d59b936ff60dca8710a62dc4b64fb
David Jaquay <djaquay@gmail.com>
2025-01-22 11:54:12 +0000

    spawn first car; car movement

--- car.gd
+++ car.gd
@@ -1,5 +1,6 @@
-extends Area2D
+extends RigidBody2D
 
+@export var const_vel: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,7 +9,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	linear_velocity = const_vel
+
 
 func start(pos) -> void:
 	position = pos
--- main.gd
+++ main.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+@export var car_scene: PackedScene
+
 const gridSize = 64
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Player.start($StartPosition.position)
+	$Player.start($PCStartPosition.position)
+	_on_car_timer_timeout()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,3 +19,16 @@ func _process(delta: float) -> void:
 		$Player.position.x += gridSize
 	if Input.is_action_just_pressed("move_left"):
 		$Player.position.x -= gridSize
+
+
+func _on_car_timer_timeout() -> void:
+	var car = car_scene.instantiate()
+	
+	var car_spawn_location = $StartLane1
+	
+	var dir = PI
+	car.position = car_spawn_location.position
+	
+	car.const_vel = Vector2(-150.0, 0.0)
+
+	add_child(car)
